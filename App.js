ca9994242c5bea5cbf80511152517c58383cb805
t1kyje00@students.oamk.fi <t1kyje00@students.oamk.fi>
2023-09-12 20:32:18 +0300

    Modified Switch exercise

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, Switch, View } from 'react-native';
 
 export default function App() {
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch =() => setIsEnabled(previousState => !previousState);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Jenni-Marian Switch</Text> EN SAA TEKSTIÄ JA SWITCHIÄ SAMALLE RIVILLE*/}
+        <View style={styles.fields}>
+          <Switch
+            trackColor={{false: '#767577', true: '#79ff58'}}
+            thumbColor={isEnabled ? '#79ff58' : '#f4f3f4'}
+            onValueChange={toggleSwitch}
+            value={isEnabled} />
+        </View>
     </View>
   );
 }
@@ -13,8 +25,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  fields: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
